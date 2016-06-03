c84a19458ca60e25b5137327270a07b8d5c74e8c
Kyle Firstenberg <kylefberg@me.com>
2016-06-03 09:03:27 -0700

    saving changes

---
+++ db/migrate/20160603004618_create_riders.rb
@@ -0,0 +1,11 @@
+class CreateRiders < ActiveRecord::Migration
+  def change
+    create_table :riders do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
