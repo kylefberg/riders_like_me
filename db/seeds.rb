f9aefc8bc93ea9205127f025f8928a794c379816
Kyle Firstenberg <kylefberg@me.com>
2016-06-06 16:39:59 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,25 @@
 Rider.destroy_all
 
-rider = Rider.new name: "JoeBob", email: "joe@joe.com", password: "abc123", password_confirmation: "abc123"
+rider = Rider.new(
+  name: "JoJo",
+  email: "joe@joe.com",
+  password: "abc123",
+  password_confirmation: "abc123"
+)
 rider.save
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+rider = Rider.new(
+  name: "JoeBob",
+  email: "joe@bob.com",
+  password: "abc123",
+  password_confirmation: "abc123"
+)
+rider.save
+
+rider = Rider.new(
+  name: "Kyle F",
+  email: "kyle@kyle.com",
+  password: "abc123",
+  password_confirmation: "abc123"
+)
+rider.save
