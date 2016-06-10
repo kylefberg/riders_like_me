8b04564fa1fa4942231f52aeaf9dd8eacc3837b9
Kyle Firstenberg <kylefberg@me.com>
2016-06-10 10:12:34 -0700

    saved seed file and now no photo entered auto photo

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -4,31 +4,29 @@ class Rider < ActiveRecord::Base
   has_many :comments_received, class_name: 'Comment', foreign_key: 'recipient_id'
   validates :email, presence: true, uniqueness: true
 
-after_initialize :set_image
-  def set_image
-    profile_photo = "/stock.jpg" unless profile_photo
-  end
   has_attached_file(
     :profile_photo,
+    default_url: '/images/nothumb.jpeg',
     styles: {
       thumb: '150x150>',
       square: '200x200#',
       medium: '300x300>'
-    },
+    }
   )
 
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
-end
 
-  def self.search rider
-    wildcarded_rider = "%#{rider}%"
-    where(
-      "name ILIKE :name OR experience ILIKE :experience OR adventure ILIKE :adventure OR street ILIKE :street OR dual_sport ILIKE :dual_sport OR dirt ILIKE :dirt",
-      name:         wildcarded_rider,
-      experience:   wildcarded_rider,
-      adventure:    wildcarded_rider,
-      street:       wildcarded_rider,
-      dual_sport:   wildcarded_rider,
-      dirt:         wildcarded_rider
-      )
-  end
+  # def self.search rider
+  #   wildcarded_rider = "%#{rider}%"
+  #   where(
+  #     "name ILIKE :name OR experience ILIKE :experience OR adventure ILIKE :adventure OR street ILIKE :street OR dual_sport ILIKE :dual_sport OR dirt ILIKE :dirt",
+  #     name:         wildcarded_rider,
+  #     experience:   wildcarded_rider,
+  #     adventure:    wildcarded_rider,
+  #     street:       wildcarded_rider,
+  #     dual_sport:   wildcarded_rider,
+  #     dirt:         wildcarded_rider
+  #     )
+  # end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Rider.destroy_all
 
 rider = Rider.new(
-  name: "Joe Smith",
+  name: "Joe Johnson",
   sex: "Male",
-  email: "joe@joe.com",
+  email: "joe@go.com",
   password: "abc123",
   password_confirmation: "abc123",
   phone: "909-111-2222",
@@ -21,7 +21,7 @@ rider.save
 rider = Rider.new(
   name: "Hanner",
   sex: "Male",
-  email: "Alaska@go.com",
+  email: "hanner@go.com",
   password: "abc123",
   password_confirmation: "abc123",
   phone: "909-111-2222",
@@ -31,7 +31,7 @@ rider = Rider.new(
   dual_sport: true,
   dirt: false,
   my_bike: "Suzuki",
-  my_bio: "I really like myself",
+  my_bio: "I have gone to Alaska by myself. It was challenging and fun and everyone should do it.",
   profile_photo: File.open(File.join(Rails.root, '/public/hanner.jpg'))
 
 )
@@ -76,16 +76,16 @@ rider.save
 rider = Rider.new(
   name: "Billy",
   sex: "Male",
-  email: "bill@go.com",
+  email: "billy@go.com",
   password: "abc123",
   password_confirmation: "abc123",
   phone: "909-111-2222",
   my_experience: "Beginner",
-  street: false,
-  adventure: true,
-  dual_sport: true,
+  street: true,
+  adventure: false,
+  dual_sport: false,
   dirt: false,
-  my_bike: "Suzuki",
+  my_bike: "Yamaha",
   my_bio: "I'm getting ready to go on an adventure'",
   profile_photo: File.open(File.join(Rails.root, '/public/bill.jpg'))
 )
@@ -106,5 +106,77 @@ rider = Rider.new(
   my_bike: "Suzuki",
   my_bio: "I would like to meet people who aren't crazy like me.",
   profile_photo: File.open(File.join(Rails.root, '/public/bride.jpg'))
+)
+rider.save
+
+rider = Rider.new(
+  name: "Bev Anderson",
+  sex: "Female",
+  email: "bev@go.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  phone: "909-111-2222",
+  my_experience: "Beginner",
+  street: false,
+  adventure: true,
+  dual_sport: true,
+  dirt: false,
+  my_bike: "Suzuki GSXR1000",
+  my_bio: "I love stunt riding but guys are intimidated by my riding skills..",
+  profile_photo: File.open(File.join(Rails.root, '/public/pink.jpg'))
+)
+rider.save
+
+rider = Rider.new(
+  name: "Alex Petroff",
+  sex: "Male",
+  email: "alex@go.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  phone: "909-111-2222",
+  my_experience: "Advanced",
+  street: true,
+  adventure: true,
+  dual_sport: true,
+  dirt: true,
+  my_bike: "Triumph",
+  my_bio: "I like riding off road and camping.",
+  profile_photo: File.open(File.join(Rails.root, '/public/triumphdirt.jpg'))
+)
+rider.save
+
+rider = Rider.new(
+  name: "Charlie Dugal",
+  sex: "Male",
+  email: "charlie@go.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  phone: "909-111-2222",
+  my_experience: "Beginner",
+  street: false,
+  adventure: true,
+  dual_sport: false,
+  dirt: false,
+  my_bike: "Honda",
+  my_bio: "Sometimes I get lost",
+  profile_photo: File.open(File.join(Rails.root, '/public/charlie.jpg'))
+)
+rider.save
+
+rider = Rider.new(
+  name: "Mary Pestrofkaski",
+  sex: "Female",
+  email: "mary@go.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  phone: "909-111-2222",
+  my_experience: "Beginner",
+  street: true,
+  adventure: true,
+  dual_sport: true,
+  dirt: false,
+  my_bike: "Kawasaki",
+  my_bio: "I just started riding and am looking for people who want to teach me.",
+  profile_photo: File.open(File.join(Rails.root, '/public/mary.jpg'))
 )
 rider.save
